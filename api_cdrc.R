705c1a9b8d6a23f251c939160b8e1887e377e6e2
Gabe Chisholm <94998324+gabechisholm@users.noreply.github.com>
2024-12-12 08:23:42 +0000

    Update api_cdrc.R

--- api_cdrc.R
+++ api_cdrc.R
@@ -1,5 +1,7 @@
 library(cdrcR)
 # Replace USERNAME and PASSWORD with your CDRC API login
+# Replace arguments in "getCDRC" to build your query
+# Minimum of two arguments is required for geographyCode, hence why Leeds is present but is removed later :P
 loginCDRC("USERNAME", "PASSWORD")
 imd <- getCDRC("IMD2019", geography = "LADname", geographyCode = c("Hartlepool", "Leeds"))
 # Removes "Leeds" as it is not needed
